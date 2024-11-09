51b14344707b554008b1e7e5e031a6fcc485f6a9
Aleem24 <md.samiulaleem16@gmail.com>
2024-11-09 19:52:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function sumOfThreeNumbers(num1,num2,num3){  
+    let sum = num1+num2+num3;
+    return sum;
+};
+total = sumOfThreeNumbers(890,678,456);
+
+document.getElementById("func1").innerHTML = "The total sum = " + total;
+
